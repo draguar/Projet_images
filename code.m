53b1ad66b1a593742c766edfc536f10fcebd212f
Blahic Noemie <nblahic@bs406-s33-06.insa-lyon.fr>
2019-12-10 11:41:41 +0100

    ajout des masques

--- code.m
+++ code.m
@@ -1,7 +1,19 @@
 close all
 volume_ref = preprocess('FLAIR.nii');
 volume_shift = preprocess('DIFFUSION.nii');
+% figure(1)
+% plot_volume(volume_ref, false);
+% figure(2)
+% plot_volume(volume_shift, false);
+
+masks_ref = volume_ref > 0;
+masks_shift = volume_shift > 0;
+
 figure(1)
-plot_volume(volume_ref, false);
-figure(2)
-plot_volume(volume_shift, false);
+plot_volume(masks_ref, true);
+figure(2);
+plot_volume(masks_ref, false);
+
+for i = 
+s = regionprops(best_contigue_ref,'centroid');
+centroids = cat(1,s.Centroid);
