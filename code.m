5571b3f621fa01240c092ba5c0147a1c331ecac3
Cluet Yvan <ycluet@bs406-s33-05.insa-lyon.fr>
2019-12-10 12:04:37 +0100

    centroides

--- code.m
+++ code.m
@@ -1,4 +1,5 @@
 close all
+clc; clear all
 volume_ref = preprocess('FLAIR.nii',1);
 volume_shift = preprocess('DIFFUSION.nii',2);
 % figure(1)
@@ -9,6 +10,16 @@ volume_shift = preprocess('DIFFUSION.nii',2);
 masks_ref = volume_ref > 0;
 masks_shift = volume_shift > 0;
 
-for i = 
-s = regionprops(best_contigue_ref,'centroid');
-centroids = cat(1,s.Centroid);
+centroids = zeros(15,2)
+for i = 1:15
+    [y, x] = ndgrid(1:size(masks_ref(:,:,i), 1), 1:size(masks_ref(:,:,i), 2));
+    centro_x = mean([x(masks_ref(:,:,i))]);
+    centro_y = mean([y(masks_ref(:,:,i))]);
+    centroids(i,:) = [centro_x, centro_y];
+end
+
+figure(1)
+plot_volume(masks_ref(:,:,12), false);
+hold on;
+plot(centroids(12,1), centroids(12,2), 'b*')
+hold off;
