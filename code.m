f1deac4b7232201dfacb294b4679fada0fedd292
Cluet Yvan <ycluet@bs406-s33-09.insa-lyon.fr>
2020-01-08 15:28:14 +0100

    presque fini

--- code.m
+++ code.m
@@ -42,7 +42,7 @@ figure(5)
 plot_rgb(volume_ref, best_resized_volume, false)
 %% SECTION 3
 %type in points from images and get coordinates
-[x_ref, y_ref, x_newvol, y_newvol] = get_points_of_interest(volume_ref, best_resized_volume);
+[x_ref, y_ref, x_newvol, y_newvol] = get_points_of_interest(volume_ref, resized_volume);
 
 
 % align these points
@@ -65,3 +65,85 @@ end
     
 plot_rgb(volume_ref, newvol_transformed, false)
 
+%% Section 4
+
+% Read two images
+optimizer = registration.optimizer.RegularStepGradientDescent;
+metric = registration.metric.MattesMutualInformation;
+
+non_rigid_volume = zeros(240,240,15);
+
+for i = 1:15
+    non_rigid_volume(:,:,i) = imregister(volume_shift(:,:,i), volume_ref(:,:,i),'affine', optimizer,metric);
+end
+
+plot_rgb(volume_ref, non_rigid_volume)
+[error, osef] = compute_error(volume_ref, non_rigid_volume);
+error
+% reduction de l'erreur importante
+
+%% test avec des carrés
+
+A = 255-rgb2gray(imread('carre1.png'));
+B = 255-rgb2gray(imread('carre2.png'));
+imshowpair(A, B);
+
+%%
+%test section 4
+C = imregister(B, A, 'affine', optimizer, metric);
+imshowpair(A,C) % validé
+
+%% test section 3 
+
+%type in points from images and get coordinates
+[x_ref, y_ref, x_newvol, y_newvol] = get_points_of_interest_test_carre(A, B);
+
+
+% align these points
+ref_ = zeros(size(x_ref, 1), 2);
+for i = 1:size(x_ref)
+    ref_(i,1) = x_ref(i);
+    ref_(i,2) = y_ref(i);
+end
+
+newvol_ = zeros(size(x_newvol, 1), 2)
+for i = 1:size(x_newvol);
+    newvol_(i,1) = x_newvol(i);
+    newvol_(i,2) = y_newvol(i);
+end
+%%
+tform = fitgeotrans(newvol_, ref_, 'NonreflectiveSimilarity')
+D = imwarp(B, tform, 'OutputView', imref2d(size(A)));
+%%
+imshowpair(A, D) %% --> validé
+%% Section 5
+image1 = round(255*mat2gray(volume_ref(:,:,6)));
+image2 = round(255*mat2gray(resized_volume(:,:,6)));
+
+sum_ = 0;
+
+for i = 1:255
+    for j = 1:255
+        p_ij = 0;
+        p_i = 0;
+        p_j = 0;
+        for a = 1:size(image1, 1)
+            for b = 1:size(image1, 2)
+                if image1(a, b) == i
+                    p_i = p_i +1;
+                end
+                if image2(a, b) == j
+                    p_j = p_j +1;
+                end
+                if image1(a, b) == i && image2(a,b) == j
+                    p_ij = p_ij + 1;
+                end
+            end
+        end
+    sum_ = sum_ + (p_ij * log(p_ij / (p_i * p_j)));
+    end
+    disp (i)
+end
+
+%%
+imshowpair(image1, image2)
---
+++ compute_error_new_method.m
@@ -0,0 +1,5 @@
+function [error] = compute_error_new_method(im1, im2)
+
+%this function takes 
+
+A = size(im1)
